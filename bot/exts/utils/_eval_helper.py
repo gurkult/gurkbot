f2bb87e42e1aa2bc10de5f51da1c495bc4fa6002
Shivansh-007 <Shivansh-007@users.noreply.github.com>
2020-12-23 12:38:28 +0530

    Do Required suggested by Vester And Vivek

--- bot/exts/utils/_eval_helper.py
+++ bot/exts/utils/_eval_helper.py
@@ -6,21 +6,23 @@ import aiohttp
 from discord.ext import commands
 from loguru import logger
 
-to_bytes = partial(bytes, encoding='utf-8')
+to_bytes = partial(bytes, encoding="utf-8")
 
 
 def _to_tio_string(couple: tuple) -> Any:
     """
-    Return a tio api compatible string, for example:
+    Return a tio api compatible string.
+
+    For example:
     (language) bash -> b'Vlang\x001\x00bash\x00'
     (code) echo "bash" -> b'F.code.tio\x0019\x00echo "bash"\n\x00'
     """
     name, obj = couple
     if not obj:
-        return b''
+        return b""
     elif isinstance(obj, list):
-        content = [f'V{name}', str(len(obj))] + obj
-        return to_bytes('\x00'.join(content) + '\x00')
+        content = [f"V{name}", str(len(obj))] + obj
+        return to_bytes("\x00".join(content) + "\x00")
     else:
         return to_bytes(f"F{name}\x00{len(to_bytes(obj))}\x00{obj}\x00")
 
@@ -28,9 +30,15 @@ def _to_tio_string(couple: tuple) -> Any:
 class Tio:
     """Helper class for eval command."""
 
-    def __init__(self, language: str, code: str, inputs: str = '',
-                 compiler_flags: Optional[list] = None, command_line_options: Optional[list] = None,
-                 args: Optional[list] = None):
+    def __init__(
+        self,
+        language: str,
+        code: str,
+        inputs: str = "",
+        compiler_flags: Optional[list] = None,
+        command_line_options: Optional[list] = None,
+        args: Optional[list] = None,
+    ):
         if args is None:
             args = []
         if command_line_options is None:
@@ -41,20 +49,23 @@ class Tio:
         self.json = "https://tio.run/languages.json"
 
         strings = {
-            'lang': [language],
-            '.code.tio': code,
-            '.input.tio': inputs,
-            'TIO_CFLAGS': compiler_flags,
-            'TIO_OPTIONS': command_line_options,
-            'args': args
+            "lang": [language],
+            ".code.tio": code,
+            ".input.tio": inputs,
+            "TIO_CFLAGS": compiler_flags,
+            "TIO_OPTIONS": command_line_options,
+            "args": args,
         }
 
-        bytes_ = b''.join(
-            map(
-                _to_tio_string, # func
-                zip(strings.keys(), strings.values()) # iterables
+        bytes_ = (
+            b"".join(
+                map(
+                    _to_tio_string,  # func
+                    zip(strings.keys(), strings.values()),  # iterables
+                )
             )
-        ) + b'R'
+            + b"R"
+        )
 
         # This returns a DEFLATE-compressed byte-string, which is what the API requires
         self.request = zlib.compress(bytes_, 9)[2:-4]
@@ -64,54 +75,59 @@ class Tio:
         async with aiohttp.ClientSession() as client_session:
             async with client_session.post(self.backend, data=self.request) as res:
                 if res.status != 200:
-                    raise aiohttp.HttpProcessingError(res.status)
+                    logger.warning(
+                        f"HttpProcessingError while getting result of code from tio api with "
+                        f"status code: {res.status}"
+                    )
 
                 data = await res.read()
-                data = data.decode('utf-8')
-                return data.replace(data[:16], '')  # remove token
+                data = data.decode("utf-8")
+                return data.replace(data[:16], "")  # remove token
 
 
 def get_raw(link: str) -> str:
     """Returns the url to raw text version of certain pastebin services."""
-    link = link.strip('<>/')  # Allow for no-embed links
+    link = link.strip("<>/")  # Allow for no-embed links
 
     authorized = (
-        'https://hastebin.com',
-        'https://gist.github.com',
-        'https://gist.githubusercontent.com'
+        "https://hastebin.com",
+        "https://gist.github.com",
+        "https://gist.githubusercontent.com",
     )
 
     if not any(link.startswith(url) for url in authorized):
-        raise commands.BadArgument(message=f"I only accept links from {', '.join(authorized)}. "
-                                           f"(Starting with 'https').")
+        raise commands.BadArgument(
+            message=f"I only accept links from {', '.join(authorized)}. "
+            f"(Starting with 'https')."
+        )
 
-    domain = link.split('/')[2]
+    domain = link.split("/")[2]
 
-    if domain == 'hastebin.com':
-        if '/raw/' in link:
+    if domain == "hastebin.com":
+        if "/raw/" in link:
             return link
-        token = link.split('/')[-1]
-        if '.' in token:
-            token = token[:token.rfind('.')]  # removes extension
-        return f'https://hastebin.com/raw/{token}'
+        token = link.split("/")[-1]
+        if "." in token:
+            token = token[: token.rfind(".")]  # removes extension
+        return f"https://hastebin.com/raw/{token}"
     else:
         # Github uses redirection so raw -> user content and no raw -> normal
         # We still need to ensure we get a raw version after this potential redirection
-        if '/raw' in link:
+        if "/raw" in link:
             return link
-        return link + '/raw'
+        return link + "/raw"
 
 
 async def paste(text: str) -> Optional[str]:
     """Upload the eval output to a paste service and return a URL to it if successful."""
     logger.info("Uploading full output to paste service...")
     async with aiohttp.ClientSession() as session:
-        post = await session.post('https://hastebin.com/documents', data=text)
+        post = await session.post("https://hastebin.com/documents", data=text)
         if post.status == 200:
             response = await post.text()
-            return f'https://hastebin.com/{response[8:-2]}'
+            return f"https://hastebin.com/{response[8:-2]}"
 
         # Rollback bin
-        post = await session.post("https://bin.drlazor.be", data={'val': text})
+        post = await session.post("https://bin.drlazor.be", data={"val": text})
         if post.status == 200:
             return post.url
--- bot/exts/utils/eval.py
+++ bot/exts/utils/eval.py
@@ -8,9 +8,9 @@ from typing import Optional
 
 import aiohttp
 from bot.bot import Bot
-from discord import Message
+from discord import Embed, Message
 from discord.ext import commands, tasks
-from discord.ext.commands import Cog, Context, command
+from discord.ext.commands import Cog, CommandError, Context, command
 from discord.utils import escape_mentions
 from loguru import logger
 from yaml import safe_load as yaml_load
@@ -25,7 +25,7 @@ class Eval(Cog):
         self.bot = bot
         with Path("bot/resources/eval/default_langs.yml").open(encoding="utf8") as file:
             self.default_languages = yaml_load(file)
-        self.languages_url = 'https://tio.run/languages.json'
+        self.languages_url = "https://tio.run/languages.json"
         self.languages = None
         self.update_languages.start()
 
@@ -36,26 +36,30 @@ class Eval(Cog):
         async with aiohttp.ClientSession() as client_session:
             async with client_session.get(self.languages_url) as response:
                 if response.status != 200:
-                    logger.warning(f"Couldn't reach languages.json (status code: {response.status}).")
+                    logger.warning(
+                        f"Couldn't reach languages.json (status code: {response.status})."
+                    )
                 languages = tuple(sorted(json.loads(await response.text())))
 
                 # Rare reassignments
                 if self.languages != languages:
                     self.languages = languages
-        logger.info(f"Successfully Updated List Of Languages To Date: {datetime.datetime.now()}")
+        logger.info(
+            f"Successfully Updated List Of Languages To Date: {datetime.datetime.now()}"
+        )
 
     wrapping = {
-        'c': '#include <stdio.h>\nint main() {code}',
-        'cpp': '#include <iostream>\nint main() {code}',
-        'cs': 'using System;class Program {static void Main(string[] args) {code}}',
-        'java': 'public class Main {public static void main(String[] args) {code}}',
-        'rust': 'fn main() {code}',
-        'd': 'import std.stdio; void main(){code}',
-        'kotlin': 'fun main(args: Array<String>) {code}'
+        "c": "#include <stdio.h>\nint main() {code}",
+        "cpp": "#include <iostream>\nint main() {code}",
+        "cs": "using System;class Program {static void Main(string[] args) {code}}",
+        "java": "public class Main {public static void main(String[] args) {code}}",
+        "rust": "fn main() {code}",
+        "d": "import std.stdio; void main(){code}",
+        "kotlin": "fun main(args: Array<String>) {code}",
     }
 
     @command(
-        help='''eval <language> [--wrapped] [--stats] <code>
+        help="""eval <language> [--wrapped] [--stats] <code>
 
             for command-line-options, compiler-flags and arguments you may
             add a line starting with this argument, and after a space add
@@ -72,12 +76,14 @@ class Eval(Cog):
 
             If the output exceeds 40 lines or Discord max message length, it will be put
             in a new hastebin and the link will be returned.
-            ''',
-        brief='Execute code in a given programming language',
+            """,
+        brief="Execute code in a given programming language",
         name="eval",
-        aliases=("e",)
+        aliases=("e",),
     )
-    async def eval_command(self, ctx: Context, language: str, *, code: str = '') -> Optional[Message]:
+    async def eval_command(
+        self, ctx: Context, language: str, *, code: str = ""
+    ) -> Optional[Message]:
         """
         Evaluate code, format it, and send the output to the corresponding channel.
 
@@ -85,47 +91,44 @@ class Eval(Cog):
         """
         logger.info(f"Received code from {ctx.author} for evaluation:\n{code}")
 
-        options = {
-            '--stats': False,
-            '--wrapped': False
-        }
-        lang = language.strip('`').lower()
+        options = {"--stats": False, "--wrapped": False}
+        lang = language.strip("`").lower()
         options_amount = len(options)
 
         # Setting options and removing them from the beginning of the command
         # options may be separated by any single whitespace, which we keep in the list
-        code = re.split(r'(\s)', code, maxsplit=options_amount)
+        code = re.split(r"(\s)", code, maxsplit=options_amount)
 
         for option in options:
-            if option in code[:options_amount * 2]:
+            if option in code[: options_amount * 2]:
                 options[option] = True
                 i = code.index(option)
                 code.pop(i)
                 code.pop(i)  # Remove following whitespace character
 
-        code = ''.join(code)
+        code = "".join(code)
 
         compiler_flags = []
         command_line_options = []
         args = []
         inputs = []
 
-        lines = code.split('\n')
+        lines = code.split("\n")
         code = []
         for line in lines:
-            if line.startswith('input '):
-                inputs.append(' '.join(line.split(' ')[1:]).strip('`'))
-            elif line.startswith('compiler-flags '):
-                compiler_flags.extend(line[15:].strip('`').split(' '))
-            elif line.startswith('command-line-options '):
-                command_line_options.extend(line[21:].strip('`').split(' '))
-            elif line.startswith('arguments '):
-                args.extend(line[10:].strip('`').split(' '))
+            if line.startswith("input "):
+                inputs.append(" ".join(line.split(" ")[1:]).strip("`"))
+            elif line.startswith("compiler-flags "):
+                compiler_flags.extend(line[15:].strip("`").split(" "))
+            elif line.startswith("command-line-options "):
+                command_line_options.extend(line[21:].strip("`").split(" "))
+            elif line.startswith("arguments "):
+                args.extend(line[10:].strip("`").split(" "))
             else:
                 code.append(line)
 
-        inputs = '\n'.join(inputs)
-        code = '\n'.join(code)
+        inputs = "\n".join(inputs)
+        code = "\n".join(code)
         text = None
 
         async with ctx.typing():
@@ -136,49 +139,58 @@ class Eval(Cog):
                     return await ctx.send("File must be smaller than 20 kio.")
                 buffer = BytesIO()
                 await ctx.message.attachments[0].save(buffer)
-                text = buffer.read().decode('utf-8')
+                text = buffer.read().decode("utf-8")
 
-            elif code.split(' ')[-1].startswith('link='):
+            elif code.split(" ")[-1].startswith("link="):
                 # Code in a paste service (gist or a hastebin link)
-                base_url = urllib.parse.quote_plus(code.split(' ')[-1][5:].strip('/'), safe=';/?:@&=$,><-[]')
+                base_url = urllib.parse.quote_plus(
+                    code.split(" ")[-1][5:].strip("/"), safe=";/?:@&=$,><-[]"
+                )
                 url = get_raw(base_url)
 
                 async with aiohttp.ClientSession() as client_session:
                     async with client_session.get(url) as response:
                         if response.status == 404:
-                            return await ctx.send('Nothing found. Check your link')
+                            return await ctx.send("Nothing found. Check your link")
                         elif response.status != 200:
-                            return await ctx.send(f'An error occurred (status code: {response.status}). '
-                                                  f'Retry later.')
+                            return await ctx.send(
+                                f"An error occurred (status code: {response.status}). "
+                                f"Retry later."
+                            )
                         text = await response.text()
                         if len(text) > 20000:
-                            return await ctx.send('Code must be shorter than 20,000 characters.')
+                            return await ctx.send(
+                                "Code must be shorter than 20,000 characters."
+                            )
 
-            elif code.strip('`'):
+            elif code.strip("`"):
                 # Code in message
-                text = code.strip('`')
+                text = code.strip("`")
                 first_line = text.splitlines()[0]
-                if re.fullmatch(r'( |[0-9A-z]*)\b', first_line):
-                    text = text[len(first_line) + 1:]
+                if re.fullmatch(r"( |[0-9A-z]*)\b", first_line):
+                    text = text[len(first_line) + 1 :]
 
             if text is None:
                 # Ensures code isn't empty after removing options
-                raise commands.MissingRequiredArgument(ctx.command.clean_params['code'])
+                logger.info(
+                    f"{ctx.author}'s job returned a MissingRequiredArgument - missing code"
+                )
+                return await ctx.send("Missing Required Argument Code.")
 
             # common identifiers, also used in highlight.js and thus discord codeblocks
             quick_map = {
-                'asm': 'assembly',
-                'c#': 'cs',
-                'c++': 'cpp',
-                'csharp': 'cs',
-                'f#': 'fs',
-                'fsharp': 'fs',
-                'js': 'javascript',
-                'nimrod': 'nim',
-                'py': 'python',
-                'q#': 'qs',
-                'rs': 'rust',
-                'sh': 'bash',
+                "asm": "assembly",
+                "c#": "cs",
+                "c++": "cpp",
+                "csharp": "cs",
+                "f#": "fs",
+                "fsharp": "fs",
+                "js": "javascript",
+                "nimrod": "nim",
+                "py": "python",
+                "q#": "qs",
+                "rs": "rust",
+                "sh": "bash",
             }
 
             if lang in quick_map:
@@ -187,7 +199,9 @@ class Eval(Cog):
             if lang in self.default_languages:
                 lang = self.default_languages[lang]
             if lang not in self.languages:
-                matches = '\n'.join([language for language in self.languages if lang in language][:10])
+                matches = "\n".join(
+                    [language for language in self.languages if lang in language][:10]
+                )
                 lang = escape_mentions(lang)
                 message = f"`{lang}` not available."
                 if matches:
@@ -195,56 +209,80 @@ class Eval(Cog):
 
                 return await ctx.send(message)
 
-            if options['--wrapped']:
-                if not (any(map(lambda x: lang.split('-')[0] == x, self.wrapping))) or lang in (
-                        'cs-mono-shell', 'cs-csi'):
-                    return await ctx.send(f'`{lang}` cannot be wrapped')
+            if options["--wrapped"]:
+                if not (
+                    any(map(lambda x: lang.split("-")[0] == x, self.wrapping))
+                ) or lang in ("cs-mono-shell", "cs-csi"):
+                    return await ctx.send(f"`{lang}` cannot be wrapped")
 
                 for beginning in self.wrapping:
-                    if lang.split('-')[0] == beginning:
-                        text = self.wrapping[beginning].replace('code', text)
+                    if lang.split("-")[0] == beginning:
+                        text = self.wrapping[beginning].replace("code", text)
                         break
 
-            tio = Tio(lang, text, compiler_flags=compiler_flags, inputs=inputs,
-                      command_line_options=command_line_options,
-                      args=args)
+            tio = Tio(
+                lang,
+                text,
+                compiler_flags=compiler_flags,
+                inputs=inputs,
+                command_line_options=command_line_options,
+                args=args,
+            )
 
             result = await tio.get_result()
 
-            if not options['--stats']:
+            if not options["--stats"]:
                 try:
                     start = result.rindex("Real time: ")
                     end = result.rindex("%\nExit code: ")
-                    result = result[:start] + result[end + 2:]
+                    result = result[:start] + result[end + 2 :]
                 except ValueError:
                     # Too much output removes this markers
                     pass
 
-            if len(result) > 1991 or result.count('\n') > 40:
+            if len(result) > 1991 or result.count("\n") > 40:
                 # If it exceeds 2000 characters (Discord longest message), counting ` and ph\n characters
                 # Or if it floods with more than 40 lines
                 # Create a hastebin and send it back
                 link = await paste(result)
 
                 if link is None:
-                    return await ctx.send("Your output was too long, but "
-                                          "I couldn't make an online bin out of it")
+                    return await ctx.send(
+                        "Your output was too long, but "
+                        "I couldn't make an online bin out of it"
+                    )
                 return await ctx.send(
-                    f'Output was too long (more than 2000 characters or 40 lines) '
-                    f'so I put it here: {link}')
+                    f"Output was too long (more than 2000 characters or 40 lines) "
+                    f"so I put it here: {link}"
+                )
 
             logger.info("Formatting output...")
 
-            zero = '\N{zero width space}'
-            result = re.sub('```', f'{zero}`{zero}`{zero}`{zero}', result)
+            zero = "\N{zero width space}"
+            result = re.sub("```", f"{zero}`{zero}`{zero}`{zero}", result)
 
-            result, exit_code = result.split('Exit code: ')
+            result, exit_code = result.split("Exit code: ")
 
-            icon = ":white_check_mark:" if exit_code == '0' else ":warning:"
+            icon = ":white_check_mark:" if exit_code == "0" else ":warning:"
             msg = f"Your eval job has completed with return code {exit_code}"
 
             logger.info(f"{ctx.author}'s job had a return code of {exit_code}")
-            await ctx.send(f'{ctx.author.mention} {icon} {msg}.\n\n```{lang}\n{result}```')
+            await ctx.send(
+                f"{ctx.author.mention} {icon} {msg}.\n\n```{lang}\n{result}```"
+            )
+
+    @eval_command.error
+    async def eval_command_error(self, ctx: Context, error: CommandError) -> None:
+        """A error handler for Eval Command."""
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(
+                title="MissingRequiredArgument",
+                description=f"Your input was invalid: {error}\n\nUsage:\n"
+                f"```{ctx.prefix}{ctx.command} {ctx.command.signature}```",
+                color=0xCD6D6D,
+            )
+            await ctx.send(embed=embed)
+            return
 
 
 def setup(bot: Bot) -> None:
