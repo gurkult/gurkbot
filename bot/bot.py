1f1b3bb4525b9253e82a560a19a53a99031500c3
Shivansh-007 <Shivansh-007@users.noreply.github.com>
2020-12-31 11:53:28 +0530

    Send message to devlog channel on startup

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 import os
 
+from discord import Embed
 from discord.ext import commands
 from loguru import logger
 
@@ -32,3 +33,14 @@ class Bot(commands.Bot):
     async def on_ready(self) -> None:
         """Ran when the bot has connected to discord and is ready."""
         logger.info("Bot online")
+        await self.startup_greeting()
+
+    async def startup_greeting(self) -> None:
+        """Announce presence to the devlog channel."""
+        embed = Embed(description="Connected!")
+        embed.set_author(
+            name="Gurk Bot",
+            url="https://github.com/gurkult/gurkbot",
+            icon_url=self.user.avatar_url
+        )
+        await self.get_channel(constants.Channels.devlog).send(embed=embed)
--- bot/constants.py
+++ bot/constants.py
@@ -2,9 +2,15 @@ import pathlib
 import os
 
 # env vars
+from typing import NamedTuple
+
 PREFIX = os.getenv("PREFIX") or "!"
 TOKEN = os.getenv("TOKEN")
 
 # paths
 EXTENSIONS = pathlib.Path("bot/exts/")
 LOG_FILE = pathlib.Path("log/gurkbot.log")
+
+
+class Channels(NamedTuple):
+    devlog = int(os.environ.get("CHANNEL_DEVLOG", 622895325144940554))
