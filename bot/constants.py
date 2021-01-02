c0b47d1c6df54e9f4e6090808dbe4127586ac98c
Arnav-2004 <arnavrangwani@gmail.com>
2021-01-02 16:26:28 +0530

    Added the id's to constants.py and this time used @commands.group and removed the client

--- bot/constants.py
+++ bot/constants.py
@@ -4,6 +4,8 @@ import os
 # env vars
 PREFIX = os.getenv("PREFIX") or "!"
 TOKEN = os.getenv("TOKEN")
+ANNOUNCEMENTS_ID = os.getenv("ANNOUNCEMENTS_ID", "789978290844598272")
+POLLS_ID = os.getenv("POLLS_ID", "790043110360350740")
 
 # paths
 EXTENSIONS = pathlib.Path("bot/exts/")
--- bot/exts/utils/subscription.py
+++ bot/exts/utils/subscription.py
@@ -1,18 +1,13 @@
 import discord
 from bot.bot import Bot
+from bot.constants import ANNOUNCEMENTS_ID, POLLS_ID
 from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Cog, Context
-from discord.utils import get
-
-client = commands.Bot(command_prefix="a!")
 
 yes_emoji = "<:yes:794231332770938901>"
 no_emoji = "⚠️"
 color = 0x32A05A
-
-announcements_id = 793864455527202849
-polls_id = 793864455527202848
 
 
 class Subscription(Cog):
@@ -21,48 +16,29 @@ class Subscription(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    @client.command()
-    async def subscribe(
-        self, ctx: Context, announcements: str = None, polls: str = None
-    ) -> None:
+    @commands.group(name="subscribe", invoke_without_command=True)
+    async def subscribe_group(self, ctx: Context, message: str = "") -> None:
         """Subscribe to announcements and polls notifications by assigning yourself the role."""
-        announcements_role = get(ctx.author.guild.roles, id=announcements_id)
-        polls_role = get(ctx.author.guild.roles, id=polls_id)
-
-        if announcements_role in ctx.author.roles and polls_role in ctx.author.roles:
-            embed = Embed(
-                title=f"{no_emoji} Already subscribed",
-                description=f"You're already subscribed to {ctx.guild}'s announcements and polls.",
-                color=color,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-            return
-
-        elif not announcements and not polls:
-            await ctx.author.add_roles(
-                discord.Object(announcements_id), reason="Subscribed to announcements"
-            )
-            await ctx.author.add_roles(
-                discord.Object(polls_id), reason="Subscribed to polls"
-            )
-            embed = Embed(
-                title=f"{yes_emoji} Subscribed",
-                description=f"You've subscribed to {ctx.guild}'s announcements and polls.",
-                color=color,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        elif announcements in ["announcements", "announcement"]:
-            if announcements_role in ctx.author.roles:
+        if message == "":
+            has_announcements = False
+            has_polls = False
+            for role in ctx.author.roles:
+                if role.id == ANNOUNCEMENTS_ID:
+                    has_announcements = True
+                if role.id == POLLS_ID:
+                    has_polls = True
+            if has_announcements and has_polls:
                 embed = Embed(
                     title=f"{no_emoji} Already subscribed",
-                    description=f"You're already subscribed to {ctx.guild}'s announcements.",
+                    description=f"You're already subscribed to {ctx.guild}'s announcements and polls.",
                     color=color,
                 )
                 await ctx.send(content=ctx.author.mention, embed=embed)
-            else:
+                return
+
+            elif has_polls:
                 await ctx.author.add_roles(
-                    discord.Object(announcements_id),
+                    discord.Object(ANNOUNCEMENTS_ID),
                     reason="Subscribed to announcements",
                 )
                 embed = Embed(
@@ -70,82 +46,75 @@ class Subscription(Cog):
                     description=f"You've subscribed to {ctx.guild}'s announcements.",
                     color=color,
                 )
-                embed = Embed(
-                    title=f"{yes_emoji} Subscribed!",
-                    description=f"You've subscribed to {ctx.guild}'s announcements.",
-                    color=color,
-                )
                 await ctx.send(content=ctx.author.mention, embed=embed)
 
-        elif polls in ["polls", "poll"]:
-            if polls_role in ctx.author.roles:
-                embed = Embed(
-                    title=f"{no_emoji} Already subscribed",
-                    description=f"You're already subscribed to {ctx.guild}'s polls.",
-                    color=color,
-                )
-                await ctx.send(content=ctx.author.mention, embed=embed)
-            else:
+            elif has_announcements:
                 await ctx.author.add_roles(
-                    discord.Object(polls_id), reason="Subscribed to polls"
+                    discord.Object(POLLS_ID),
+                    reason="Subscribed to polls",
                 )
                 embed = Embed(
                     title=f"{yes_emoji} Subscribed",
                     description=f"You've subscribed to {ctx.guild}'s polls.",
                     color=color,
                 )
-                embed = Embed(
-                    title=f"{yes_emoji} subscribed",
-                    description=f"You've subscribed to {ctx.guild}'s polls.",
-                    color=color,
-                )
                 await ctx.send(content=ctx.author.mention, embed=embed)
 
-    @client.command()
-    async def unsubscribe(
-        self, ctx: Context, announcements: str = None, polls: str = None
-    ) -> None:
-        """Unsubscribe to announcements and polls notifications by unassigning yourself the role."""
-        announcements_role = get(ctx.author.guild.roles, id=announcements_id)
-        polls_role = get(ctx.author.guild.roles, id=polls_id)
-
-        if (
-            announcements_role not in ctx.author.roles
-            and polls_role not in ctx.author.roles
-        ):
-            embed = Embed(
-                title=f"{no_emoji} Already unsubscribed",
-                description=f"You're already unsubscribed to {ctx.guild}'s announcements and polls.",
-                color=color,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-            return
-
-        elif not announcements and not polls:
-            await ctx.author.remove_roles(
-                discord.Object(announcements_id), reason="Unsubscribed to announcements"
-            )
-            await ctx.author.remove_roles(
-                discord.Object(polls_id), reason="Unsubscribed to polls"
-            )
-            embed = Embed(
-                title=f"{yes_emoji} Unsubscribed",
-                description=f"You've unsubscribed to {ctx.guild}'s announcements and polls notifications.",
-                color=color,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        elif announcements in ["announcements", "announcement"]:
-            if announcements_role not in ctx.author.roles:
-                embed = Embed(
-                    title=f"{no_emoji} Already unsubscribed",
-                    description=f"You're already unsubscribed to {ctx.guild}'s announcements.",
-                    color=color,
-                )
-                await ctx.send(content=ctx.author.mention, embed=embed)
             else:
+                await ctx.author.add_roles(
+                    discord.Object(ANNOUNCEMENTS_ID),
+                    reason="Subscribed to announcements",
+                )
+                await ctx.author.add_roles(
+                    discord.Object(POLLS_ID), reason="Subscribed to polls"
+                )
+                embed = Embed(
+                    title=f"{yes_emoji} Subscribed",
+                    description=f"You've subscribed to {ctx.guild}'s announcements and polls.",
+                    color=color,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+
+    @commands.group(name="unsubscribe", invoke_without_command=True)
+    async def unsubscribe_group(self, ctx: Context, message: str = "") -> None:
+        """Unsubscribe to announcements and polls notifications by assigning yourself the role."""
+        if message == "":
+            has_announcements = False
+            has_polls = False
+            for role in ctx.author.roles:
+                if role.id == ANNOUNCEMENTS_ID:
+                    has_announcements = True
+                if role.id == POLLS_ID:
+                    has_polls = True
+            if has_announcements and has_polls:
                 await ctx.author.remove_roles(
-                    discord.Object(announcements_id),
+                    discord.Object(ANNOUNCEMENTS_ID),
+                    reason="Unsubscribed to announcements",
+                )
+                await ctx.author.remove_roles(
+                    discord.Object(POLLS_ID), reason="Unsubscribed to polls"
+                )
+                embed = Embed(
+                    title=f"{yes_emoji} Unsubscribed",
+                    description=f"You've unsubscribed to {ctx.guild}'s announcements and polls.",
+                    color=color,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+
+            elif has_polls:
+                await ctx.author.remove_roles(
+                    discord.Object(POLLS_ID), reason="Unsubscribed to polls"
+                )
+                embed = Embed(
+                    title=f"{yes_emoji} Unsubscribed",
+                    description=f"You've unsubscribed to {ctx.guild}'s polls.",
+                    color=color,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+
+            elif has_announcements:
+                await ctx.author.remove_roles(
+                    discord.Object(ANNOUNCEMENTS_ID),
                     reason="Unsubscribed to announcements",
                 )
                 embed = Embed(
@@ -155,24 +124,109 @@ class Subscription(Cog):
                 )
                 await ctx.send(content=ctx.author.mention, embed=embed)
 
-        elif ctx.author.content == "a!polls" or ctx.author.content == "a!poll":
-            if polls_role not in ctx.author.roles:
-                embed = Embed(
-                    title=f"{no_emoji} Already unsubscribed!",
-                    description=f"You're already unsubscribed to {ctx.guild}'s polls.",
-                    color=color,
-                )
-                await ctx.send(content=ctx.author.mention, embed=embed)
             else:
-                await ctx.author.remove_roles(
-                    discord.Object(polls_id), reason="Unsubscribed to polls"
-                )
                 embed = Embed(
-                    title=f"{yes_emoji} Unsubscribed",
-                    description=f"You've unsubscribed to {ctx.guild}'s polls.",
+                    title=f"{no_emoji} Already unsubscribed",
+                    description=f"You're already unsubscribed to {ctx.guild}'s announcements and polls.",
                     color=color,
                 )
                 await ctx.send(content=ctx.author.mention, embed=embed)
+
+    @subscribe_group.command(name="announcements", aliases=("announcement",))
+    async def announcements_subscribe(self, ctx: Context, *_) -> None:
+        """Subscribe to announcements by assigning yourself the role."""
+        for role in ctx.author.roles:
+            if role.id == ANNOUNCEMENTS_ID:
+                embed = Embed(
+                    title=f"{no_emoji} Already subscribed",
+                    description=f"You're already subscribed to {ctx.guild}'s announcements.",
+                    color=color,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+                return
+        await ctx.author.add_roles(
+            discord.Object(ANNOUNCEMENTS_ID), reason="Subscribed to announcements"
+        )
+        embed = Embed(
+            title=f"{yes_emoji} Subscribed",
+            description=f"You've subscribed to {ctx.guild}'s announcements.",
+            color=color,
+        )
+        await ctx.send(content=ctx.author.mention, embed=embed)
+
+    @unsubscribe_group.command(name="announcements", aliases=("announcement",))
+    async def announcements_unsubscribe(self, ctx: Context, *_) -> None:
+        """Unsubscribe to announcements by unassigning yourself the role."""
+        has_role = False
+        for role in ctx.author.roles:
+            if role.id == ANNOUNCEMENTS_ID:
+                has_role = True
+                break
+        if not has_role:
+            embed = Embed(
+                title=f"{no_emoji} Already unsubscribed",
+                description=f"You're already unsubscribed to {ctx.guild}'s announcements.",
+                color=color,
+            )
+            await ctx.send(content=ctx.author.mention, embed=embed)
+            return
+        await ctx.author.remove_roles(
+            discord.Object(ANNOUNCEMENTS_ID), reason="Unsubscribed to announcements"
+        )
+        embed = Embed(
+            title=f"{yes_emoji} Unsubscribed",
+            description=f"You've unsubscribed to {ctx.guild}'s announcements.",
+            color=color,
+        )
+        await ctx.send(content=ctx.author.mention, embed=embed)
+
+    @subscribe_group.command(name="polls", aliases=("poll",))
+    async def polls_subscribe(self, ctx: Context, *_) -> None:
+        """Subscribe to polls by assigning yourself the role."""
+        for role in ctx.author.roles:
+            if role.id == POLLS_ID:
+                embed = Embed(
+                    title=f"{no_emoji} Already subscribed",
+                    description=f"You're already subscribed to {ctx.guild}'s polls.",
+                    color=color,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+                return
+        await ctx.author.add_roles(
+            discord.Object(POLLS_ID), reason="Subscribed to polls"
+        )
+        embed = Embed(
+            title=f"{yes_emoji} Subscribed",
+            description=f"You've subscribed to {ctx.guild}'s polls.",
+            color=color,
+        )
+        await ctx.send(content=ctx.author.mention, embed=embed)
+
+    @unsubscribe_group.command(name="polls", aliases=("poll",))
+    async def polls_unsubscribe(self, ctx: Context, *_) -> None:
+        """Unsubscribe to polls by unassigning yourself the role."""
+        has_role = False
+        for role in ctx.author.roles:
+            if role.id == POLLS_ID:
+                has_role = True
+                break
+        if not has_role:
+            embed = Embed(
+                title=f"{no_emoji} Already unsubscribed",
+                description=f"You're already unsubscribed to {ctx.guild}'s polls.",
+                color=color,
+            )
+            await ctx.send(content=ctx.author.mention, embed=embed)
+            return
+        await ctx.author.remove_roles(
+            discord.Object(POLLS_ID), reason="Unsubscribed to polls"
+        )
+        embed = Embed(
+            title=f"{yes_emoji} Unsubscribed",
+            description=f"You've unsubscribed to {ctx.guild}'s polls.",
+            color=color,
+        )
+        await ctx.send(content=ctx.author.mention, embed=embed)
 
 
 def setup(bot: Bot) -> None:
