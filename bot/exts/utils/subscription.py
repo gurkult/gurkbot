7c4b6f9f52c05c9a9ad57249d70748ec53860554
Arnav-2004 <arnavrangwani@gmail.com>
2021-01-04 16:31:14 +0530

    added subscribe_group and unsubscribe_group helper

--- bot/exts/utils/subscription.py
+++ bot/exts/utils/subscription.py
@@ -11,6 +11,120 @@ class Subscription(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
+    async def sub_unsub_helper(
+        self,
+        ctx: Context,
+        sub_unsub: str,
+        role_id1: int = Roles.announcements,
+        role_id2: int = Roles.polls,
+        role_name1: str = "announcements",
+        role_name2: str = "polls",
+    ) -> None:
+        """Subscribe group and Unsubscribe group helper."""
+        has_announcements = False
+        has_polls = False
+        for role in ctx.author.roles:
+            if role.id == role_id1:
+                has_announcements = True
+            if role.id == role_id2:
+                has_polls = True
+        if has_announcements and has_polls:
+            if sub_unsub == "Subscribed":
+                embed = Embed(
+                    title=f"{Emojis.warning_emoji} Already {sub_unsub.lower()}",
+                    description=f"""You're already {sub_unsub.lower()} to {ctx.guild}'s
+                    {role_name1} and {role_name2}.""",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+                return
+            else:
+                await ctx.author.remove_roles(
+                    ctx.guild.get_role(role_id1),
+                    reason=f"{sub_unsub} to {role_name1}",
+                )
+                await ctx.author.remove_roles(
+                    ctx.guild.get_role(role_id2), reason=f"{sub_unsub} to {role_name2}"
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"""You've {sub_unsub.lower()} to {ctx.guild}'s
+                    {role_name1} and {role_name2}.""",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+
+        elif has_polls:
+            if sub_unsub == "Subscribed":
+                await ctx.author.add_roles(
+                    ctx.guild.get_role(role_id1),
+                    reason=f"{sub_unsub} to {role_name1}",
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"You've {sub_unsub.lower()} to {ctx.guild}'s {role_name1}.",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+            else:
+                await ctx.author.remove_roles(
+                    ctx.guild.get_role(role_id2),
+                    reason=f"{sub_unsub} to {role_name2}",
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"You've {sub_unsub.lower()} to {ctx.guild}'s {role_name2}.",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+        elif has_announcements:
+            if sub_unsub == "Subscribed":
+                await ctx.author.add_roles(
+                    ctx.guild.get_role(role_id2),
+                    reason=f"{sub_unsub} to {role_name2}",
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"You've {sub_unsub.lower()} to {ctx.guild}'s {role_name2}.",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+            else:
+                await ctx.author.remove_roles(
+                    ctx.guild.get_role(role_id1),
+                    reason=f"{sub_unsub} to {role_name1}",
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"You've {sub_unsub.lower()} to {ctx.guild}'s {role_name1}.",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+        else:
+            if sub_unsub == "Subscribed":
+                await ctx.author.add_roles(
+                    ctx.guild.get_role(role_id1),
+                    reason=f"{sub_unsub} to {role_name1}",
+                )
+                await ctx.author.add_roles(
+                    ctx.guild.get_role(role_id2), reason=f"{sub_unsub} to {role_name2}"
+                )
+                embed = Embed(
+                    title=f"{Emojis.confirmation_emoji} {sub_unsub}",
+                    description=f"""You've {sub_unsub.lower()} to {ctx.guild}'s
+                    {role_name1} and {role_name2}.""",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+            else:
+                embed = Embed(
+                    title=f"{Emojis.warning_emoji} Already {sub_unsub.lower()}",
+                    description=f"""You're already {sub_unsub.lower()} to {ctx.guild}'s
+                    {role_name1} and {role_name2}.""",
+                    color=Colors.green,
+                )
+                await ctx.send(content=ctx.author.mention, embed=embed)
+
     async def subscribe_helper(
         self, ctx: Context, role_id: int, role_name: str
     ) -> None:
@@ -66,116 +180,12 @@ class Subscription(Cog):
     @commands.group(name="subscribe", invoke_without_command=True)
     async def subscribe_group(self, ctx: Context) -> None:
         """Subscribe to announcements and polls notifications by assigning yourself the role."""
-        has_announcements = False
-        has_polls = False
-        for role in ctx.author.roles:
-            if role.id == Roles.announcements:
-                has_announcements = True
-            if role.id == Roles.polls:
-                has_polls = True
-        if has_announcements and has_polls:
-            embed = Embed(
-                title=f"{Emojis.warning_emoji} Already subscribed",
-                description=f"You're already subscribed to {ctx.guild}'s announcements and polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-            return
-
-        elif has_polls:
-            await ctx.author.add_roles(
-                ctx.guild.get_role(Roles.announcements),
-                reason="Subscribed to announcements",
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Subscribed",
-                description=f"You've subscribed to {ctx.guild}'s announcements.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        elif has_announcements:
-            await ctx.author.add_roles(
-                ctx.guild.get_role(Roles.polls),
-                reason="Subscribed to polls",
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Subscribed",
-                description=f"You've subscribed to {ctx.guild}'s polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        else:
-            await ctx.author.add_roles(
-                ctx.guild.get_role(Roles.announcements),
-                reason="Subscribed to announcements",
-            )
-            await ctx.author.add_roles(
-                ctx.guild.get_role(Roles.polls), reason="Subscribed to polls"
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Subscribed",
-                description=f"You've subscribed to {ctx.guild}'s announcements and polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
+        await self.sub_unsub_helper(ctx, "Subscribed")
 
     @commands.group(name="unsubscribe", invoke_without_command=True)
     async def unsubscribe_group(self, ctx: Context) -> None:
         """Unsubscribe to announcements and polls notifications by assigning yourself the role."""
-        has_announcements = False
-        has_polls = False
-        for role in ctx.author.roles:
-            if role.id == Roles.announcements:
-                has_announcements = True
-            if role.id == Roles.polls:
-                has_polls = True
-        if has_announcements and has_polls:
-            await ctx.author.remove_roles(
-                ctx.guild.get_role(Roles.announcements),
-                reason="Unsubscribed to announcements",
-            )
-            await ctx.author.remove_roles(
-                ctx.guild.get_role(Roles.polls), reason="Unsubscribed to polls"
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Unsubscribed",
-                description=f"You've unsubscribed to {ctx.guild}'s announcements and polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        elif has_polls:
-            await ctx.author.remove_roles(
-                ctx.guild.get_role(Roles.polls), reason="Unsubscribed to polls"
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Unsubscribed",
-                description=f"You've unsubscribed to {ctx.guild}'s polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        elif has_announcements:
-            await ctx.author.remove_roles(
-                ctx.guild.get_role(Roles.announcements),
-                reason="Unsubscribed to announcements",
-            )
-            embed = Embed(
-                title=f"{Emojis.confirmation_emoji} Unsubscribed",
-                description=f"You've unsubscribed to {ctx.guild}'s announcements.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
-
-        else:
-            embed = Embed(
-                title=f"{Emojis.warning_emoji} Already unsubscribed",
-                description=f"You're already unsubscribed to {ctx.guild}'s announcements and polls.",
-                color=Colors.green,
-            )
-            await ctx.send(content=ctx.author.mention, embed=embed)
+        await self.sub_unsub_helper(ctx, "Unsubscribed")
 
     @subscribe_group.command(name="announcements", aliases=("announcement",))
     async def announcements_subscribe(self, ctx: Context) -> None:
