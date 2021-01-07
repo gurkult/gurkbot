2e2d67593bab5aef2b78d51001c86f3a59555b49
saiTama-max <vinamraj01@gmail.com>
2021-01-07 19:42:50 +0530

    lower the params to fix bugs

--- bot/utils/is_gurkan.py
+++ bot/utils/is_gurkan.py
@@ -5,9 +5,9 @@ from fuzzywuzzy import process
 
 def gurkan_check(target: str) -> bool:
     """Returns a bool stating if the name given is a gurkan or not."""
-    return bool(re.search(r"gurk|urkan", target))
+    return bool(re.search(r"gurk|urkan", target.lower()))
 
 
 def gurkan_rate(name: str) -> int:
     """Returns the rate of gurkan in the name given."""
-    return process.extractOne("gurkan", [name])[1]
+    return process.extractOne("gurkan", [name.lower()])[1]
