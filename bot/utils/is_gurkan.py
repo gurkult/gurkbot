a79762f8ce0336cbaa3f59c2529e5b0a1d4eab1e
saiTama-max <vinamraj01@gmail.com>
2021-01-07 18:05:52 +0530

    Made seperate file named is_gurkan in utils for gurkan_rate and gurkan_check command

---
+++ bot/utils/is_gurkan.py
@@ -0,0 +1,13 @@
+import re
+
+from fuzzywuzzy import process
+
+
+def gurkan_check(target: str) -> bool:
+    """Returns a bool stating if the name given is a gurkan or not."""
+    return bool(re.search(r"gurk|urkan", target))
+
+def gurkan_rate(name: str) -> int:
+    """Returns the rate of gurkan in the name given."""
+    return process.extractOne("gurkan", [name])[1]
+
