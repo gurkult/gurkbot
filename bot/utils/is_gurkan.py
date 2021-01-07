71b5a30e73182177ca2f4bf14973b6f0a0559c7e
saiTama-max <vinamraj01@gmail.com>
2021-01-07 18:07:04 +0530

    Fix linting

--- bot/utils/is_gurkan.py
+++ bot/utils/is_gurkan.py
@@ -7,7 +7,7 @@ def gurkan_check(target: str) -> bool:
     """Returns a bool stating if the name given is a gurkan or not."""
     return bool(re.search(r"gurk|urkan", target))
 
+
 def gurkan_rate(name: str) -> int:
     """Returns the rate of gurkan in the name given."""
     return process.extractOne("gurkan", [name])[1]
-
