4a9ab40544d0dfe3c384704826d0cb3965507dc5
saiTama-max <vinamraj01@gmail.com>
2021-01-08 10:47:39 +0530

    Using different method for rate with regex only

--- bot/utils/is_gurkan.py
+++ bot/utils/is_gurkan.py
@@ -1,13 +1,13 @@
 import re
 
-from fuzzywuzzy import process
-
 
 def gurkan_check(target: str) -> bool:
     """Returns a bool stating if the name given is a gurkan or not."""
-    return bool(re.search(r"gurk|urkan", target))
+    return bool(re.search(r"gurk|urkan", target.lower()))
 
 
 def gurkan_rate(name: str) -> int:
     """Returns the rate of gurkan in the name given."""
-    return process.extractOne("gurkan", [name])[1]
+    search = re.search(r"gurk|urkan", name.lower())
+    span = search.span() if search else (0, 0)
+    return int((span[1] - span[0]) / len(name) * 100)
