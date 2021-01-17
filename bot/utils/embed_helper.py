7da80054942485211e09ef34e300c5b48efc73ff
codephile1221 <vivekashok1221@gmail.com>
2021-01-17 12:45:40 +0400

    add information embed helper

---
+++ bot/utils/embed_helper.py
@@ -0,0 +1,41 @@
+from random import choice
+from typing import Optional, Tuple, Union
+
+from discord import Embed
+
+from ..constants import Colours, POSITIVE_REPLIES
+
+
+class EmbedHelper:
+    """doctstring."""
+
+    @staticmethod
+    def embed_helper(
+        title: str,
+        description: str,
+        colour: int,
+        url: Optional[str] = None,
+    ) -> Embed:
+        """Method that creates an embed."""
+        embed = Embed(title=title, description=description, colour=colour, url=url)
+
+        return embed
+
+    @classmethod
+    def information(
+        cls,
+        *,
+        title: Optional[str] = None,
+        description: str,
+        url: Optional[str] = None,
+        **fields: Union[str, Tuple[str, bool]]
+    ) -> Embed:
+        """Embed used in a positive context."""
+        title_ = title or choice(POSITIVE_REPLIES)
+
+        return cls.embed_helper(
+            title=title_,
+            description=description,
+            url=url,
+            colour=Colours.green,
+        )
