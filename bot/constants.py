009cbd1ad8ee8ef32eacfe38a60d8960d3469320
Shivansh-007 <Shivansh-007@users.noreply.github.com>
2021-01-28 04:50:04 +0530

    Add tic tac toe command

--- bot/constants.py
+++ bot/constants.py
@@ -25,6 +25,25 @@ class Channels(NamedTuple):
     devlog = int(os.getenv("CHANNEL_DEVLOG", 789431367167377448))
 
 
+class Emojis:
+    # TicTacToe Emojis
+    number_emojis = {
+        1: "\u0031\ufe0f\u20e3",
+        2: "\u0032\ufe0f\u20e3",
+        3: "\u0033\ufe0f\u20e3",
+        4: "\u0034\ufe0f\u20e3",
+        5: "\u0035\ufe0f\u20e3",
+        6: "\u0036\ufe0f\u20e3",
+        7: "\u0037\ufe0f\u20e3",
+        8: "\u0038\ufe0f\u20e3",
+        9: "\u0039\ufe0f\u20e3",
+    }
+    confirmation = "\u2705"
+    decline = "\u274c"
+    cucumber = ":cucumber:"
+    watermelon = ":watermelon:"
+
+
 # Bot replies
 with pathlib.Path("bot/resources/bot_replies.yml").open(encoding="utf8") as file:
     bot_replies = yaml.safe_load(file)
