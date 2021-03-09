42cf27ab6236de0bbada063e1cf8eacac9d2495c
Shakya Majumdar <shakyathegreat.majumdar@gmail.com>
2021-03-09 11:04:26 +0530

    Send gif in embed

--- bot/exts/fun/bonker.py
+++ bot/exts/fun/bonker.py
@@ -90,7 +90,9 @@ class Bonk(commands.Cog):
             with concurrent.futures.ProcessPoolExecutor() as pool:
                 await asyncio.get_running_loop().run_in_executor(pool, func)
 
-        await ctx.send(file=discord.File("out.gif"))
+        embed = discord.Embed(title=f'get bonkt {member.display_name}')
+        embed.set_image(url="attachment://out.gif")
+        await ctx.send(file=discord.File("out.gif"), embed=embed)
 
 
 def setup(bot: commands.Bot) -> None:
