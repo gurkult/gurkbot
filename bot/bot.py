e163a5201708d9f502c45fd3e1d25bfd28b39dcb
Rohan <rohanjnr44@gmail.com>
2021-04-15 20:07:33 +0530

    Handle database connection failure.
    Send dev notification on discord when database connection fails and
    disconnect the bot.

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from aiohttp import ClientSession
 from bot.postgres import db_init
@@ -19,9 +20,29 @@ class Bot(commands.Bot):
         intents.presences = True
 
         self.http_session = ClientSession()
-        self.db_pool = None
 
         super().__init__(command_prefix=constants.PREFIX, intents=intents)
+
+        self.loop.create_task(self._db_setup())
+
+        self.load_extensions()
+
+    async def _db_setup(self) -> None:
+        """Setup and initialize database connection."""
+        try:
+            await db_init()
+        except Exception as e:
+            logger.info("Initializing database...FAILED...closing bot connection.")
+            await self.wait_until_ready()
+            error_msg = f"**{e.__class__.__name__}**\n```{e}```"
+            embed = Embed(
+                title="Database error",
+                description=error_msg,
+                colour=constants.Colours.soft_red,
+            )
+
+            await self.notify_dev_alert(embed=embed)
+            await self.close()
 
     def load_extensions(self) -> None:
         """Load all the extensions in the exts/ folder."""
@@ -30,7 +51,6 @@ class Bot(commands.Bot):
             if extension.name.startswith("_"):
                 continue  # ignore files starting with _
             dot_path = str(extension).replace(os.sep, ".")[:-3]  # remove the .py
-
             self.load_extension(dot_path)
             logger.info(f"Successfully loaded extension:  {dot_path}")
 
@@ -46,14 +66,7 @@ class Bot(commands.Bot):
     async def on_ready(self) -> None:
         """Ran when the bot has connected to discord and is ready."""
         logger.info("Bot online")
-        self.load_extensions()
-
         await self.startup_greeting()
-
-    async def login(self, *args, **kwargs) -> None:
-        """Setup database before logging into discord."""
-        await db_init()
-        await super().login(*args, **kwargs)
 
     async def startup_greeting(self) -> None:
         """Announce presence to the devlog channel."""
@@ -63,11 +76,18 @@ class Bot(commands.Bot):
         )
         await self.get_channel(constants.Channels.devlog).send(embed=embed)
 
+    async def notify_dev_alert(
+        self, content: Optional[str] = None, embed: Optional[Embed] = None
+    ) -> None:
+        """Notify dev alert channel."""
+        await self.get_channel(constants.Channels.devalerts).send(
+            content=content, embed=embed
+        )
+
     async def close(self) -> None:
         """Close Http session when bot is shutting down."""
-        await super().close()
-
         if self.http_session:
             await self.http_session.close()
 
         await Tortoise.close_connections()
+        await super().close()
