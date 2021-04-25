bb15d92a6628723c3284964c36578c19891c4418
rohan <rohanjnr44@gmail.com>
2021-04-25 21:33:46 +0530

    Add Off Topic embed helper.

--- bot/exts/fun/off_topic.py
+++ bot/exts/fun/off_topic.py
@@ -41,81 +41,6 @@ class OffTopicNames(Cog):
             await db_fetch(self.bot.db_pool, "SELECT * FROM offtopicnames")
         )
 
-    @group(name="offtopicnames", aliases=("otn",), invoke_without_command=True)
-    async def off_topic_names(self, ctx: Context) -> None:
-        """Commands for managing off-topic-channel names."""
-        await ctx.send_help(ctx.command)
-
-    @off_topic_names.command(name="add", aliases=("a",))
-    async def add_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
-        """Add off topic channel name."""
-        if name in self.ot_names:
-            await ctx.send(f":x:`{name}` already exists!")
-            return
-
-        similar_names = self._find(name)
-
-        if similar_names:
-            embed = self._find_embed_builder(
-                similar_names[:5], "Found similar existing names :name_badge:"
-            )
-            embed.set_footer(text="Do you still want to add the off topic name?")
-            confirmation_msg = await ctx.send(embed=embed)
-
-            await confirmation_msg.add_reaction(constants.Emojis.CHECK_MARK_EMOJI)
-            await confirmation_msg.add_reaction(constants.Emojis.CROSS_MARK_EMOJI)
-
-            def check(reaction: Reaction, user: User) -> bool:
-                return reaction.message == confirmation_msg and user == ctx.author
-
-            async def _exit() -> None:
-                await confirmation_msg.delete()
-                await ctx.send(f"Off topic name `{name}` not added.")
-
-            try:
-                reaction, _ = await self.bot.wait_for(
-                    "reaction_add", check=check, timeout=60.0
-                )
-            except asyncio.TimeoutError:
-                return await _exit()
-
-            if str(reaction.emoji) == constants.Emojis.CROSS_MARK_EMOJI:
-                return await _exit()
-
-        await db_execute(
-            self.bot.db_pool, "INSERT INTO offtopicnames VALUES ($1)", name
-        )
-        self.ot_names[name] = 0
-
-        await ctx.send(f":ok_hand: `{name}` has been added!")
-
-    @off_topic_names.command(name="delete", aliases=("d", "r", "remove"))
-    async def delete_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
-        """Delete off topic channel name."""
-        if name not in self.ot_names:
-            await ctx.send(f":x: `{name}` not found!")
-            if names := self._find(name):
-                await self._send_paginated_embed(
-                    ctx, names, "Did you mean one of the following?"
-                )
-            return
-
-        await db_execute(
-            self.bot.db_pool, "DELETE FROM offtopicnames WHERE name=$1", name
-        )
-        del self.ot_names[name]
-
-        await ctx.send(f"`{name}` has been deleted :white_check_mark:")
-
-    @off_topic_names.command(name="find", aliases=("f", "s", "search"))
-    async def find_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
-        """Find similar off-topic names in database."""
-        await self._send_paginated_embed(
-            ctx,
-            self._find(name),
-            f"{constants.Emojis.MAG_RIGHT_EMOJI} Search result: {name}",
-        )
-
     def _find(self, name: OT_Converter) -> List[str]:
         """Find similar Off-topic names."""
         return [
@@ -140,11 +65,6 @@ class OffTopicNames(Cog):
             colour=constants.Colours.green if ot_names else constants.Colours.soft_red,
         )
 
-    @off_topic_names.command(name="list", aliases=("l",))
-    async def list_ot_names(self, ctx: Context) -> None:
-        """List all Off Topic names."""
-        await self._send_paginated_embed(ctx, self.ot_names.keys(), "Off Topic Names")
-
     @staticmethod
     async def _send_paginated_embed(
         ctx: Context, lines: Iterable[str], title: str
@@ -159,6 +79,102 @@ class OffTopicNames(Cog):
             allow_empty_lines=True,
         )
 
+    @staticmethod
+    async def _send_ot_embed(
+        ctx: Context, description: str, positive: bool, title: str = "Off Topic Names"
+    ) -> None:
+        """Embed helper for Off Topic Names cog."""
+        embed = Embed(
+            title=title,
+            description=description,
+            colour=constants.Colours.green if positive else constants.Colours.soft_red,
+        )
+        await ctx.send(embed=embed)
+
+    @group(name="offtopicnames", aliases=("otn",), invoke_without_command=True)
+    async def off_topic_names(self, ctx: Context) -> None:
+        """Commands for managing off-topic-channel names."""
+        await ctx.send_help(ctx.command)
+
+    @off_topic_names.command(name="add", aliases=("a",))
+    async def add_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
+        """Add off topic channel name."""
+        if name in self.ot_names:
+            await self._send_ot_embed(ctx, f":x:`{name}` already exists!", False)
+            return
+
+        similar_names = self._find(name)
+
+        if similar_names:
+            embed = self._find_embed_builder(
+                similar_names[:5], "Found similar existing names :name_badge:"
+            )
+            embed.set_footer(text="Do you still want to add the off topic name?")
+            confirmation_msg = await ctx.send(embed=embed)
+
+            await confirmation_msg.add_reaction(constants.Emojis.CHECK_MARK_EMOJI)
+            await confirmation_msg.add_reaction(constants.Emojis.CROSS_MARK_EMOJI)
+
+            def check(reaction: Reaction, user: User) -> bool:
+                return reaction.message == confirmation_msg and user == ctx.author
+
+            async def _exit() -> None:
+                await confirmation_msg.delete()
+                await self._send_ot_embed(
+                    ctx, f"Off topic name `{name}` not added.", False
+                )
+
+            try:
+                reaction, _ = await self.bot.wait_for(
+                    "reaction_add", check=check, timeout=60.0
+                )
+            except asyncio.TimeoutError:
+                return await _exit()
+
+            if str(reaction.emoji) == constants.Emojis.CROSS_MARK_EMOJI:
+                return await _exit()
+
+        await db_execute(
+            self.bot.db_pool, "INSERT INTO offtopicnames VALUES ($1)", name
+        )
+        self.ot_names[name] = 0
+
+        await self._send_ot_embed(ctx, f"`{name}` has been added :ok_hand:", True)
+
+    @off_topic_names.command(name="delete", aliases=("d", "r", "remove"))
+    async def delete_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
+        """Delete off topic channel name."""
+        if name not in self.ot_names:
+            await ctx.send(f":x: `{name}` not found!")
+            if names := self._find(name):
+                await self._send_paginated_embed(
+                    ctx, names, "Did you mean one of the following?"
+                )
+            return
+
+        await db_execute(
+            self.bot.db_pool, "DELETE FROM offtopicnames WHERE name=$1", name
+        )
+        del self.ot_names[name]
+
+        await self._send_ot_embed(
+            ctx, f"`{name}` has been deleted :white_check_mark:", True
+        )
+
+    @off_topic_names.command(name="find", aliases=("f", "s", "search"))
+    async def find_ot_name(self, ctx: Context, *, name: OT_Converter) -> None:
+        """Find similar off-topic names in database."""
+        await self._send_paginated_embed(
+            ctx,
+            self._find(name),
+            f"{constants.Emojis.MAG_RIGHT_EMOJI} Search result: {name}",
+        )
+
+    @off_topic_names.command(name="list", aliases=("l",))
+    async def list_ot_names(self, ctx: Context) -> None:
+        """List all Off Topic names."""
+        await self._send_paginated_embed(ctx, self.ot_names.keys(), "Off Topic Names")
+
     async def update_ot_channel_name(self) -> None:
         """Update ot-channel name everyday at midnight."""
         while not self.bot.is_closed():
@@ -168,7 +184,7 @@ class OffTopicNames(Cog):
             )
             till_midnight = int((midnight_datetime - now).total_seconds())
             logger.info(f"Waiting {till_midnight}s before re-naming off topic channel.")
-            await asyncio.sleep(till_midnight)
+            await asyncio.sleep(30)
 
             # Algorithm to select next off topic name based on usage/number of times the name has been used.
             # Least used names have a higher chance of being selected.
