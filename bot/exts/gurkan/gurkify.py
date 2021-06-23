a2ed78d0f0035b3a0970b777744beddebc022d8e
Arnav-2004 <70969910+Arnav-2004@users.noreply.github.com>
2021-06-23 08:27:28 +0530

    fix linting

--- bot/exts/gurkan/gurkify.py
+++ bot/exts/gurkan/gurkify.py
@@ -1,8 +1,9 @@
 import random
+
+from bot.bot import Bot
+from bot.constants import Colours, Gurks, NEGATIVE_REPLIES
 from discord import Embed
 from discord.ext import commands
-from bot.constants import Gurks, NEGATIVE_REPLIES, Colours, Emojis
-from bot.bot import Bot
 
 
 class Gurkify(commands.Cog):
@@ -10,24 +11,25 @@ class Gurkify(commands.Cog):
 
     @commands.command(name="gurkify")
     async def gurkify(self, ctx: commands.Context) -> None:
-        """Gurkifying users display name"""
+        """Gurkifying users display name."""
         display_name = ctx.author.display_name
 
         if len(display_name) > 26:
             embed = Embed(
-                    title=f"{Emojis.CROSS_MARK_EMOJI} No",
-                    description=(
-                        "Your display name is too long to be gurkified. "
-                        "Please change it to be under 26 characters."
-                    ),
-                    color=Colours.soft_red
-                )
+                title=f"{random.choice(NEGATIVE_REPLIES)}",
+                description=(
+                    "Your nick name is too long to be gurkified. "
+                    "Please change it to be under 26 characters."
+                ),
+                color=Colours.soft_red,
+            )
             await ctx.send(embed=embed)
             return
         else:
             display_name += random.choice(Gurks.gurks)
             await ctx.author.edit(nick=display_name)
             await ctx.send("Done!")
+
 
 def setup(bot: Bot) -> None:
     """Loads the gurkify cog."""
