c3d4e55a46c2f270634b3e0c58242ed447a78a3b
RohanJnr <rohanjnr44@gmail.com>
2021-06-27 16:48:13 +0530

    Store message jump url instead of url, channel ID for reminders.

--- bot/postgres/tables/reminders.sql
+++ bot/postgres/tables/reminders.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS reminders (
-    reminder_id serial NOT NULL,
-    message_id BIGINT PRIMARY KEY,
+    reminder_id serial NOT NULL PRIMARY KEY,
+    jump_url TEXT UNIQUE,
     user_id BIGINT NOT NULL,
+    channel_id BIGINT NOT NULL,
     end_time TIMESTAMP NOT NULL,
     content TEXT DEFAULT NULL,
-    loop_seconds BIGINT DEFAULT NULL,
-    sent BOOLEAN DEFAULT FALSE
+    loop_seconds BIGINT DEFAULT NULL
 );
