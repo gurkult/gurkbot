58869e3f8bc273008cbd8a9ea743658152dd5e42
aphkyle <81857274+aphkyle@users.noreply.github.com>
2021-08-22 14:44:30 +0800

    fix error

--- bot/exts/gurkcraft/gurkcraft.py
+++ bot/exts/gurkcraft/gurkcraft.py
@@ -2,6 +2,7 @@ import discord
 from typing import NoReturn
 from discord.ext import commands
 from discord.ext.commands import Bot
+from ...constants import MC_SERVER, MC_PORT, Colours
 
 from mcstatus import MinecraftServer
 
@@ -11,25 +12,22 @@ class Minecraft(commands.Cog):
 
     def __init__(self, bot: Bot) -> NoReturn:
         self.bot = bot
-        self.server = MinecraftServer("129.159.251.229", 25565)
+        self.server = MinecraftServer(MC_SERVER, MC_PORT)
 
     @commands.command()
-    async def mcstatus(self, ctx: discord.Context) -> None:
+    async def mcstatus(self, ctx: commands.Context) -> None:
         """Collects data from minecraft server"""
         status = self.server.status()
         players = [user["name"] for user in status.raw["players"]["sample"]]
         embed_value = f"""\
         Server               : mc.gurkult.com
-
         Server Latency       : {status.latency}ms
-
         Gurkans Online       : {status.players.online}
-
-        "Gurkans Connected: {", ".join(players) if status.players.online else ""}\
+        Gurkans Connected: {", ".join(players) if status.players.online else ""}\
         """
 
         embed = discord.Embed(
-            title="Gurkcraft", description=embed_value, color=0x1F8B4C
+            title="Gurkcraft", description=embed_value, color=Colours.green
         )
         await ctx.send(embed=embed)
 
