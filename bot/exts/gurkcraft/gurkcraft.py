0e70543843c887cfdb839586d21714a5e476db12
aphkyle <81857274+aphkyle@users.noreply.github.com>
2021-08-23 19:00:26 +0800

    Update gurkcraft.py

--- bot/exts/gurkcraft/gurkcraft.py
+++ bot/exts/gurkcraft/gurkcraft.py
@@ -18,16 +18,13 @@ class Minecraft(commands.Cog):
         """Collects data from minecraft server"""
         status = self.server.status()
         players = [user["name"] for user in status.raw["players"]["sample"]]
-        embed_value = f"""\
-        Server: mc.gurkult.com
-        Server Latency: {status.latency}ms
-        Gurkans Online: {status.players.online}
-        Gurkans Connected: {", ".join(players) if status.players.online else ""}\
-        """
-
         embed = discord.Embed(
             title="Gurkcraft", description=embed_value, color=Colours.green
         )
+        embed.add_field(name="Server", value="mc.gurkult.com")
+        embed.add_field(name="Server Latency", value=f"{status.latency}ms")
+        embed.add_field(name="Gurkans Online", value=status.players.online)
+        embed.add_field(name="Gurkans Connected", value=", ".join(players))
         await ctx.send(embed=embed)
 
 
