2122f67331c5f1153dae7b6246c27e886dd3ea12
onerandomusername <genericusername414+git@gmail.com>
2021-12-15 01:13:05 -0500

    feat: added extension management commands
    took these from my own project, removed some features that weren't relevent/part of the spec
    includes commands to load, reload, or unload extensions
    commands are role-locked to the steering council

---
+++ bot/utils/extensions.py
@@ -0,0 +1,24 @@
+import os
+from typing import Generator
+
+from bot import constants
+from loguru import logger
+
+
+EXTENSIONS: list[str] = []
+"""All extensions."""
+
+
+def unqualify(name: str) -> str:
+    """Return an unqualified name given a qualified module/package `name`."""
+    return name.rsplit(".", maxsplit=1)[-1]
+
+
+def walk_extensions() -> Generator[str, None, None]:
+    """Yield extensions from the configured constants.EXTENSIONS subpackage."""
+    for extension in constants.EXTENSIONS.glob("*/*.py"):
+        if extension.name.startswith("_"):
+            continue  # ignore files starting with _
+        dot_path = str(extension).replace(os.sep, ".")[:-3]  # remove the .py
+        yield dot_path
+        logger.info(f"Successfully loaded extension:  {dot_path}")
