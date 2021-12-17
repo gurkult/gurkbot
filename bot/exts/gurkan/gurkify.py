426b06b0c399beddb7d6644f3c483a823a82a636
Gustav Odinger <gustav@Gustavs-MBP.lan>
2021-12-17 03:22:10 +0100

    Add error handling for Forbidden, "already gurkan"

--- bot/exts/gurkan/gurkify.py
+++ bot/exts/gurkan/gurkify.py
@@ -1,8 +1,9 @@
 import random
 
 from bot.bot import Bot
-from bot.constants import Colours, Gurks, NEGATIVE_REPLIES, POSITIVE_REPLIES
-from discord import Embed
+from bot.constants import Colours, GurkanNameEndings, NEGATIVE_REPLIES, POSITIVE_REPLIES
+from bot.utils.is_gurkan import gurkan_check
+from discord import Embed, Forbidden
 from discord.ext import commands
 
 
@@ -14,6 +15,18 @@ class Gurkify(commands.Cog):
         """Gurkify user's display name."""
         display_name = ctx.author.display_name
 
+        if gurkan_check(display_name):
+            embed = Embed(
+                title="I love the ambition...",
+                description=(
+                    "... but you're already a gurkan! Instead of becoming a 'double-gurkan', "
+                    "why not focus on living a truly gurkan life instead?"
+                ),
+                color=Colours.yellow,
+            )
+            await ctx.send(content=ctx.author.mention, embed=embed)
+            return
+
         if len(display_name) > 26:
             embed = Embed(
                 title=random.choice(NEGATIVE_REPLIES),
@@ -23,16 +36,27 @@ class Gurkify(commands.Cog):
                 ),
                 color=Colours.soft_red,
             )
-            await ctx.send(embed=embed)
-        else:
-            display_name += random.choice(Gurks.gurks)
+            await ctx.send(content=ctx.author.mention, embed=embed)
+            return
+        display_name += random.choice(GurkanNameEndings.name_endings)
+
+        try:
             await ctx.author.edit(nick=display_name)
+        except Forbidden:
             embed = Embed(
-                title=random.choice(POSITIVE_REPLIES),
-                description="You nick name has been gurkified.",
-                color=Colours.green,
+                title="You're too powerful!",
+                description="I can't change the names of users with top roles higher than mine.",
+                color=Colours.soft_red,
             )
             await ctx.send(content=ctx.author.mention, embed=embed)
+            return
+
+        embed = Embed(
+            title=random.choice(POSITIVE_REPLIES),
+            description="You nick name has been gurkified.",
+            color=Colours.green,
+        )
+        await ctx.send(content=ctx.author.mention, embed=embed)
 
 
 def setup(bot: Bot) -> None:
